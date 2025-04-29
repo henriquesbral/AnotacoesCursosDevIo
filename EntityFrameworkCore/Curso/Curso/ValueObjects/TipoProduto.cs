8d22cc85220f2b3d9277de081c0c555ceb8ccd21
Carlos Henrique <henriquesbral@gmail.com>
2025-04-28 23:31:11 -0300

    Subindo novas classes para o projeto de pedidos

---
+++ EntityFrameworkCore/Curso/Curso/ValueObjects/TipoProduto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso.ValueObjects
+{
+    public enum TipoProduto
+    {
+        MercadoriaParaRevenda,
+        Embalagem,
+        Serviço,
+    }
+}
