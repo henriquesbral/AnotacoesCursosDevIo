338d20edc169eb8ee5cd9ab7e18e4bc5deb22b5d
Carlos Henrique <henriquesbral@gmail.com>
2025-04-28 23:52:23 -0300

    Subindo novos ajuste e a classe DBContext

---
+++ EntityFrameworkCore/Curso/Curso/Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LENOVOHENRIQUE\\SQLEXPRESS;Database=DesenvolvedorIO;User Id=DevCursoIO;Password=Aps@2025140612#1;");
+        }
+    }
+}
