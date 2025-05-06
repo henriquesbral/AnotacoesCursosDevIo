3a4cef82f425e26780a860fdd825744c078b4840
Carlos Henrique <henriquesbral@gmail.com>
2025-05-05 23:59:23 -0300

    Adicionado a linha que chama todas as classes configuration.

--- EntityFrameworkCore/Curso/Curso/Data/ApplicationContext.cs
+++ EntityFrameworkCore/Curso/Curso/Data/ApplicationContext.cs
@@ -16,51 +16,7 @@ namespace Curso.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Cliente>(p =>
-            {
-                p.ToTable("Cliente");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
-                p.Property(p => p.Telefone).HasColumnType("CHAR(11)");
-                p.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
-                p.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
-                p.Property(p => p.Cidade).HasMaxLength(60).IsRequired();
-
-                p.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone"); //Criando um indice dentro da tabela 
-            });
-
-            modelBuilder.Entity<Produto>(p =>
-            {
-                p.ToTable("Produto");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.CodigoBarras).HasColumnType("VARCHAR(14)").IsRequired();
-                p.Property(p => p.Descricao).HasColumnType("VARCHAR(60)");
-                p.Property(p => p.Valor).IsRequired();
-                p.Property(p => p.TipoProduto).HasConversion<string>();
-            });
-
-            modelBuilder.Entity<Pedido>(p =>
-            {
-                p.ToTable("Pedido");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.IniciadoEm).HasDefaultValue("GETDATE()").ValueGeneratedOnAdd(); //Por default irá salvar a data hora atual
-                p.Property(p => p.Status).HasConversion<string>();
-                p.Property(p => p.TipoFrete).HasConversion<int>();
-                p.Property(p => p.Observacao).HasColumnType("VARCHAR(512)");
-
-                p.HasMany(p => p.Itens) //Configura o relacionamento entre as tabelas muitos pra um 
-                    .WithOne(p => p.Pedido)
-                    .OnDelete(DeleteBehavior.Cascade);
-            });
-
-            modelBuilder.Entity<PedidoItem>(p =>
-            {
-                p.ToTable("PedidoItens");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.Quantidade).HasDefaultValue().IsRequired(); //irá preencher auto quando nao informar
-                p.Property(p => p.Valor).IsRequired();
-                p.Property(p => p.Desconto).IsRequired();
-            });
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
         }
     }
 }
