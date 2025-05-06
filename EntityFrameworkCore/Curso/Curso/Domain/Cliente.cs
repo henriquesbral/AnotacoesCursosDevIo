fa212194c4a961aa8ff80f3f4e8f0b241ed582a6
Carlos Henrique <henriquesbral@gmail.com>
2025-05-06 00:07:09 -0300

    Subindo Data Annotations

--- EntityFrameworkCore/Curso/Curso/Domain/Cliente.cs
+++ EntityFrameworkCore/Curso/Curso/Domain/Cliente.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Curso.Domain
 {
+    [Table("Clientes")]
     public class Cliente
     {
+        [Key]
         public int Id { get; set; }
-
+        [Required]
         public string Nome { get; set; }
-
+        //[Column("Phone")]
         public string Telefone { get; set; }
 
         public string CEP { get; set; }
